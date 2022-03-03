11c0de7ce45597dd32c383fb20834aad9046f2bf
AnkithMahindrakar <ankith.mahindrakar@popcornapps.com>
2022-03-03 11:29:45 +0530

    Initial Commit

---
+++ VSR_iOS_Native/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VSR_iOS_Native
+//
+//  Created by Ankith Mahindrakar on 03/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
