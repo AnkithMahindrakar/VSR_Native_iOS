1dfb3e52662bc17cea19645c90aac09c02508421
AnkithMahindrakar <ankith.mahindrakar@popcornapps.com>
2022-03-07 09:49:39 +0530

    Initail commit with splash Screen,login Screen, Profile Screen

--- VSR_iOS_Native/ViewController.swift
+++ VSR_iOS_Native/ViewController.swift
@@ -1,19 +1,13 @@
-//
-//  ViewController.swift
-//  VSR_iOS_Native
-//
-//  Created by Ankith Mahindrakar on 03/03/22.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    
 }
-
